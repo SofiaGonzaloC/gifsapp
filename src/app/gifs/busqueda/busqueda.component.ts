1e208c377c8db8b99a425a168cd1668eddace06b
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2022-03-29 19:47:19 -0700

    Clase 81. @Viewchild - Obtener referencias a objetos del HTML

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value;
 
-  ngOnInit(): void {
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
