fdc76c18ca101018b3f3276a2928638acb95f99a
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2022-04-05 18:06:07 -0600

    Clase 83. Controlar el historial de búsquedas

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,8 +10,14 @@ export class GifsService {
     return [...this._historial];
   }
 
-  buscarGifs(query: string){
-    this._historial.unshift(query);
+  buscarGifs(query: string = ''){
+
+    query = query.trim().toLowerCase(); // Mantiene el valor siempre en minuscula
+
+    if(!this._historial.includes(query)){ // Checa si ya esta el valor por agregar
+      this._historial.unshift(query)
+      this._historial = this._historial.splice(0, 10); // Solo obtiene los ultimos 10 resultados
+    }
 
     console.log(this._historial)
   }
